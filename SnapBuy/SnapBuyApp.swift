9d5e30ce54fc6498df7f02962549e6b7e505ba82
Minhgaa <bminh.cv@gmail.com>
2025-03-11 23:38:19 +0700

    Initial Commit

---
+++ SnapBuy/SnapBuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapBuyApp.swift
+//  SnapBuy
+//
+//  Created by minhgaa on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SnapBuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
