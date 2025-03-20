4890b43be57d89c2074e7fef409dbe19db9d4833
Minhgaa <bminh.cv@gmail.com>
2025-03-21 01:53:22 +0700

    Add baseview, basepopup, basesheet, tabbar

---
+++ SnapBuy/Source/Modules/MyUtils/Types.swift
@@ -0,0 +1,16 @@
+//
+//  Types.swift
+//  TabBarAnimation
+//
+//  Created by Thanh Hoang on 17/2/25.
+//
+
+import SwiftUI
+
+var screenWidth: CGFloat {
+    UIApplication.shared.screenWidth
+}
+
+var screenHeight: CGFloat {
+    UIApplication.shared.screenHeight
+}
