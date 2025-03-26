38b99596bda5fd5ae207bfc97ee1c17b0b3710af
hungle <186537386+hungwleeMoniq@users.noreply.github.com>
2025-03-26 09:26:14 +0700

    add login screen

--- SnapBuy/Source/Helpers/SBComponentProvider.swift
+++ SnapBuy/Source/Helpers/SBComponentProvider.swift
@@ -1,12 +1,5 @@
 import SwiftUI
 
 class SBComponentProvider {
-    let shared: SBComponentProvider
-    private init() {
-        shared = .init()
-    }
     
-    func createButton() -> any View {
-        
-    }
 }
--- SnapBuy/Source/Modules/LoginScreen/Subviews/SBTextInput.swift
+++ SnapBuy/Source/Modules/LoginScreen/Subviews/SBTextInput.swift
@@ -5,23 +5,32 @@ struct SBTextField: View {
     let placeholder: String
     @Binding var text: String
     var isSecure: Bool = false
+    @FocusState private var isFocused: Bool
     
     var body: some View {
         HStack {
             image
-                .foregroundColor(.gray)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 24, height: 24)
                 .padding(.leading, 10)
             
             if isSecure {
                 SecureField(placeholder, text: $text)
                     .padding()
+                    .focused($isFocused)
             } else {
                 TextField(placeholder, text: $text)
                     .padding()
+                    .focused($isFocused)
             }
         }
         .frame(height: 50)
-        .background(Color.gray.opacity(0.1))
+        .background(text.isEmpty && !isFocused ? Color.gray.opacity(0.1) : Color.clear)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(isFocused ? Color.main : (text.isEmpty ? Color.clear : Color.black), lineWidth: 2)
+        )
         .cornerRadius(10)
         .padding(.horizontal, 20)
     }
